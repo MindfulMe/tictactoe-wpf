7560fcf444feca3d7a427b60356a3968542e716f
Jack Raider                              (Rostislav Gogolauri) <go.go.gg.rostislav@gmail.com>
2018-01-22 23:54:07 +0200

    Add files via upload

--- TicTacToeWPF/Constants.cs
+++ TicTacToeWPF/Constants.cs
@@ -1,20 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.IO;
+using System.Drawing;
 namespace TicTacToeWPF
-{
+{       
     class Constants
-    {
+    {   
         // Indices in combo box
         public const int HOT_SEAT_MODE = 0;
         public const int AI_EASY_MODE = 1;
         public const int AI_HARD_MODE = 2;
-
+              
+        public Image X_SYMBOL = Image.FromFile(@"3.png");
         // Player marks
-        public const string O_SYMBOL = "O";
-        public const string X_SYMBOL = "X";
+        public const string O_SYMBOL = "0";
+      //  public const string X = "X";
     }
 }
